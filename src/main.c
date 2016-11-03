430c0749aaaecb7a0680a5e781e38b6e5a442ea9
Brandon Garling <bjg96@nau.edu>
2016-11-03 10:52:48 -0700

    Add sample project code

--- src/main.c
+++ src/main.c
@@ -1,96 +1,195 @@
 #include <GLFW/glfw3.h>
+
 #include <stdlib.h>
 #include <stdio.h>
-static const struct
-{
-    float x, y;
-    float r, g, b;
-} vertices[3] =
-{
-    { -0.6f, -0.4f, 1.f, 0.f, 0.f },
-    {  0.6f, -0.4f, 0.f, 1.f, 0.f },
-    {   0.f,  0.6f, 0.f, 0.f, 1.f }
+
+
+GLFWwindow* window;
+
+
+typedef struct {
+    float position[3];
+    float color[4];
+} Vertex;
+
+
+const Vertex Vertices[] = {
+        {{1, -1, 0}, {1, 0, 0, 1}},
+        {{1, 1, 0}, {0, 1, 0, 1}},
+        {{-1, 1, 0}, {0, 0, 1, 1}},
+        {{-1, -1, 0}, {0, 0, 0, 1}}
 };
-static const char* vertex_shader_text =
-"uniform mat4 MVP;\n"
-"attribute vec3 vCol;\n"
-"attribute vec2 vPos;\n"
-"varying vec3 color;\n"
-"void main()\n"
-"{\n"
-"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
-"    color = vCol;\n"
-"}\n";
-static const char* fragment_shader_text =
-"varying vec3 color;\n"
-"void main()\n"
-"{\n"
-"    gl_FragColor = vec4(color, 1.0);\n"
-"}\n";
-static void error_callback(int error, const char* description)
-{
-    fprintf(stderr, "Error: %s\n", description);
+
+
+const GLubyte Indices[] = {
+        0, 1, 2,
+        2, 3, 0
+};
+
+
+char* vertex_shader_src =
+        "attribute vec4 Position;\n"
+                "attribute vec4 SourceColor;\n"
+                "\n"
+                "varying vec4 DestinationColor;\n"
+                "\n"
+                "void main(void) {\n"
+                "    DestinationColor = SourceColor;\n"
+                "    gl_Position = Position;\n"
+                "}\n";
+
+
+char* fragment_shader_src =
+        "varying vec4 DestinationColor;\n"
+                "\n"
+                "void main(void) {\n"
+                "    gl_FragColor = DestinationColor;\n"
+                "}\n";
+
+
+GLint simple_shader(GLint shader_type, char* shader_src) {
+
+    GLint compile_success = 0;
+
+    GLuint shader_id = glCreateShader(shader_type);
+
+    glShaderSource(shader_id, 1, &shader_src, 0);
+
+    printf("===\n");
+    printf("%s\n", shader_src);
+    printf("===\n");
+
+    glCompileShader(shader_id);
+
+    glGetShaderiv(shader_id, GL_COMPILE_STATUS, &compile_success);
+
+    if (compile_success == GL_FALSE) {
+        GLchar message[256];
+        glGetShaderInfoLog(shader_id, sizeof(message), 0, &message[0]);
+        printf("glCompileShader Error: %s\n", message);
+        exit(1);
+    }
+
+    return shader_id;
 }
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
+
+
+int simple_program() {
+
+    GLint link_success = 0;
+
+    GLint program_id = glCreateProgram();
+    GLint vertex_shader = simple_shader(GL_VERTEX_SHADER, vertex_shader_src);
+    GLint fragment_shader = simple_shader(GL_FRAGMENT_SHADER, fragment_shader_src);
+
+    glAttachShader(program_id, vertex_shader);
+    glAttachShader(program_id, fragment_shader);
+
+    glLinkProgram(program_id);
+
+    glGetProgramiv(program_id, GL_LINK_STATUS, &link_success);
+
+    if (link_success == GL_FALSE) {
+        GLchar message[256];
+        glGetProgramInfoLog(program_id, sizeof(message), 0, &message[0]);
+        printf("glLinkProgram Error: %s\n", message);
+        exit(1);
+    }
+
+    return program_id;
 }
-int main(void)
-{
-    GLFWwindow* window;
-    GLuint vertex_buffer, vertex_shader, fragment_shader, program;
-    GLint mvp_location, vpos_location, vcol_location;
+
+
+static void error_callback(int error, const char* description) {
+    fputs(description, stderr);
+}
+
+
+int main(void) {
+
+    GLint program_id;
+    GLuint color_slot;
+    GLuint position_slot;
+    GLuint vertex_buffer;
+    GLuint index_buffer;
+
     glfwSetErrorCallback(error_callback);
+
+    // Initialize GLFW library
     if (!glfwInit())
-        exit(EXIT_FAILURE);
+        return -1;
+
+    glfwDefaultWindowHints();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
-    if (!window)
-    {
+
+    // Create and open a window
+    window = glfwCreateWindow(640,
+                              480,
+                              "Hello World",
+                              NULL,
+                              NULL);
+
+    if (!window) {
         glfwTerminate();
-        exit(EXIT_FAILURE);
+        printf("glfwCreateWindow Error\n");
+        exit(1);
     }
-    glfwSetKeyCallback(window, key_callback);
+
     glfwMakeContextCurrent(window);
-    glfwSwapInterval(1);
-    // NOTE: OpenGL error checks have been omitted for brevity
+
+    program_id = simple_program();
+
+    glUseProgram(program_id);
+
+    position_slot = glGetAttribLocation(program_id, "Position");
+    color_slot = glGetAttribLocation(program_id, "SourceColor");
+    glEnableVertexAttribArray(position_slot);
+    glEnableVertexAttribArray(color_slot);
+
+    // Create Buffer
     glGenBuffers(1, &vertex_buffer);
+
+    // Map GL_ARRAY_BUFFER to this buffer
     glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
-    glCompileShader(vertex_shader);
-    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
-    glCompileShader(fragment_shader);
-    program = glCreateProgram();
-    glAttachShader(program, vertex_shader);
-    glAttachShader(program, fragment_shader);
-    glLinkProgram(program);
-    mvp_location = glGetUniformLocation(program, "MVP");
-    vpos_location = glGetAttribLocation(program, "vPos");
-    vcol_location = glGetAttribLocation(program, "vCol");
-    glEnableVertexAttribArray(vpos_location);
-    glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
-                          sizeof(float) * 5, (void*) 0);
-    glEnableVertexAttribArray(vcol_location);
-    glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
-                          sizeof(float) * 5, (void*) (sizeof(float) * 2));
-    while (!glfwWindowShouldClose(window))
-    {
-        float ratio;
-        int width, height;
-        glfwGetFramebufferSize(window, &width, &height);
-        ratio = width / (float) height;
-        glViewport(0, 0, width, height);
+
+    // Send the data
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+
+    glGenBuffers(1, &index_buffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
+
+    // Repeat
+    while (!glfwWindowShouldClose(window)) {
+
+        glClearColor(0, 104.0/255.0, 55.0/255.0, 1.0);
         glClear(GL_COLOR_BUFFER_BIT);
-        glUseProgram(program);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glViewport(0, 0, 640, 480);
+
+        glVertexAttribPointer(position_slot,
+                              3,
+                              GL_FLOAT,
+                              GL_FALSE,
+                              sizeof(Vertex),
+                              0);
+
+        glVertexAttribPointer(color_slot,
+                              4,
+                              GL_FLOAT,
+                              GL_FALSE,
+                              sizeof(Vertex),
+                              (GLvoid*) (sizeof(float) * 3));
+
+        glDrawElements(GL_TRIANGLES,
+                       sizeof(Indices) / sizeof(GLubyte),
+                       GL_UNSIGNED_BYTE, 0);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
     glfwDestroyWindow(window);
     glfwTerminate();
     exit(EXIT_SUCCESS);
