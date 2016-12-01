3b2e77dec807ddd31607b734dcb8100e018f2a7a
Brandon Garling <bjg96@nau.edu>
2016-12-01 10:30:50 -0700

    Add magnification filter specification

--- src/main.c
+++ src/main.c
@@ -799,6 +799,7 @@ int main (int argc, char *argv[]) {
     glGenTextures(1, &tex);
     glBindTexture(GL_TEXTURE_2D, tex);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image.width, image.height, 0, GL_RGB, GL_FLOAT, image.pixmap);
 
     glVertexAttribPointer(position_slot,
